7a20214f294a0d684936fd26bdb501a51874e112
Jordan Tyner <jdtyner@icloud.com>
2018-05-29 23:22:14 -0700

    removed extra whitespace

--- src/main/java/codeu/model/store/basic/UserStore.java
+++ src/main/java/codeu/model/store/basic/UserStore.java
@@ -58,8 +58,6 @@ public class UserStore {
   /** The in-memory list of Users. */
   private List<User> users;
 
-
-
   /** This class is a singleton, so its constructor is private. Call getInstance() instead. */
   private UserStore(PersistentStorageAgent persistentStorageAgent) {
     this.persistentStorageAgent = persistentStorageAgent;
