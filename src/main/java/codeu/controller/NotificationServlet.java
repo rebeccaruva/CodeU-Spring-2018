2587c3ae450d3ce63fdab6486ee0f9a367e15554
Jordan Tyner <jdtyner@icloud.com>
2018-06-15 23:29:37 -0700

    notification servlet class

---
+++ src/main/java/codeu/controller/NotificationServlet.java
@@ -0,0 +1,21 @@
+package codeu.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*Servlet class responsible for Notification class functions */
+public class NotificationServlet extends HttpServlet {
+
+  /**
+   * This function fires when a user requests the /notifications URL. It simply forwards the request to
+   * notifications.jsp.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    request.getRequestDispatcher("/WEB-INF/view/notifications.jsp").forward(request, response);
+  }
+}
